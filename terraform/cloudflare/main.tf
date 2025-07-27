2d8fd098744efb965dabdef11754af5c05381b22
yuandrk <yurii.andriuk@gmail.com>
2025-07-27 19:28:07 +0100

    chore: refactor Cloudflare tunnel module to support multiple services and update variable structure

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -1,14 +1,31 @@
 data "cloudflare_zone" "root" {
-  zone_id = var.cloudflare_zone_id      
+  zone_id = var.cloudflare_zone_id
 }
 
-module "pihole_tunnel" {
-  source     = "../modules/tunnel"
+module "homelab_tunnel" {
+  source = "../modules/tunnel"
 
-  account_id = var.cloudflare_account_id
-  zone_id    = data.cloudflare_zone.root.zone_id
-
+  account_id         = var.cloudflare_account_id
+  zone_id            = data.cloudflare_zone.root.zone_id
   existing_tunnel_id = "4a6abf9a-d178-4a56-9586-a3d77907c5f1"
-  hostname           = "pihole.yuandrk.net"
-  service            = "http://127.0.0.1:80"
+
+  services = {
+    pihole = {
+      hostname = "pihole.yuandrk.net"
+      service  = "http://127.0.0.1:80"
+    }
+    budget = {
+      hostname = "budget.yuandrk.net"
+      service  = "http://127.0.0.1:5006"
+    }
+    # easy to add new like: 
+    # grafana = {
+    #   hostname = "grafana.yuandrk.net"
+    #   service  = "http://127.0.0.1:3000"
+    # }
+    # prometheus = {
+    #   hostname = "prometheus.yuandrk.net"
+    #   service  = "http://127.0.0.1:9090"
+    # }
+  }
 }
--- terraform/cloudflare/versions.tf
+++ terraform/cloudflare/versions.tf
@@ -1,8 +1,8 @@
 terraform {
   required_version = ">= 1.8.0, < 2.0"
   required_providers {
-    cloudflare = { 
-      source  = "cloudflare/cloudflare" 
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
       version = ">= 5.3.0, < 6.0"
     }
   }
