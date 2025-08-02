de8a3e0584efe87249edd8f095cc95c76406b597
yuandrk <yurii.andriuk@gmail.com>
2025-08-02 19:01:04 +0100

    fix: update Pi-hole Cloudflare tunnel port from 80 to 8081
    Resolves port conflict with K3s Traefik LoadBalancer that was causing
    external access issues to pihole.yuandrk.net

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -8,7 +8,7 @@ locals {
     {
       name     = "pihole"
       hostname = "pihole.yuandrk.net"
-      service  = "http://127.0.0.1:80"
+      service  = "http://127.0.0.1:8081"
     },
     {
       name     = "budget"
