68f53e9e7356467bd31b083eadd863ff2c34400d
yuandrk <yurii.andriuk@gmail.com>
2025-08-17 11:54:51 +0100

    remove: completely remove Authentik from homelab setup
    - Delete Authentik namespace and all K8s resources
    - Remove Authentik application directory and configurations
    - Remove Authentik encrypted secrets and references
    - Remove auth.yuandrk.net from Cloudflare tunnel configuration
    - Update Terraform to remove DNS records and tunnel routing

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -50,11 +50,6 @@ locals {
       hostname = "pgadmin.yuandrk.net"
       service  = "http://k3s-master:80"
     },
-    {
-      name     = "auth"
-      hostname = "auth.yuandrk.net"
-      service  = "http://k3s-master:80"
-    }
   ]
   
   #
